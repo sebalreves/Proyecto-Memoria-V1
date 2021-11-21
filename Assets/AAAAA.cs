7c36ad4d1b7942b973026e56f97bac9e208e8793
Sebastian Munoz <sebalreves@gmail.com>
2021-11-21 03:33:18 -0300

    autocomplete fixed

--- Assets/AAAAA.cs
+++ Assets/AAAAA.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AAAAA : MonoBehaviour {
-    // Start is called before the first frame update 
-    void Start()  {
+
+    void Start() {
 
     }
 
