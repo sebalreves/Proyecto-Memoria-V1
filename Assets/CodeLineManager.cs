e9b6a131aa7a513a96d46687d7c4095ad5928b0a
Sebastian Munoz <sebalreves@gmail.com>
2022-02-16 17:54:10 -0300

    animacion ejecucion codigo de boton

--- Assets/CodeLineManager.cs
+++ Assets/CodeLineManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
@@ -11,7 +12,8 @@ public class CodeLineManager : MonoBehaviour {
     public static CodeLineManager _instance;
     GameObject linePrefab;
     public Color amarillo_1, amarillo_2, verde;
-
+    public AnimationCurve fillAnimationCurve;
+    public AnimationCurve fillAlphaAnimationCurve;
 
 
     private IEnumerator Start() {
@@ -33,7 +35,7 @@ public class CodeLineManager : MonoBehaviour {
     }
 
 
-    public void trySetColorLine(int _fromLineIndex, int _toLineIndex, Color _color) {
+    public void trySetColorLine(int _fromLineIndex, int _toLineIndex, Color _color, float _time = 1f, Action _action = null) {
         if (_fromLineIndex > _toLineIndex) {
             Debug.LogWarning("_fromline parameter > toLine");
         }
@@ -45,26 +47,86 @@ public class CodeLineManager : MonoBehaviour {
                 Debug.LogWarning("Line not found" + e);
                 return;
             }
-            codeLine.GetComponent<Image>().color = _color;
+            // codeLine.GetComponent<Image>().color = _color;
+            try {
+                StartCoroutine(fillCodeLine(codeLine, _color, _time, _action));
+                StartCoroutine(executeLine(_time, _action));
+            } catch (System.Exception) {
+
+                Debug.LogWarning("Error al animar linea");
+            }
         }
     }
 
-    public void trySetColorLine(int _lineIndex, Color _color) {
+
+
+    public void trySetColorLine(int _lineIndex, Color _color, float _time = 1f, Action _action = null) {
         GameObject codeLine;
         try {
             codeLine = codeContainer.transform.GetChild(_lineIndex).gameObject;
         } catch (System.Exception e) {
-            Debug.LogWarning("Line not found" + e);
+            // Debug.LogWarning("Line not found" + e);
             return;
         }
-        codeLine.GetComponent<Image>().color = _color;
+        // codeLine.GetComponent<Image>().color = _color;
+        try {
+            StartCoroutine(fillCodeLine(codeLine, _color, _time, _action));
+            StartCoroutine(executeLine(_time, _action));
+        } catch (System.Exception) {
+
+            Debug.LogWarning("Error al animar linea");
+        }
+    }
+
+    public IEnumerator executeLine(float _time, Action _action) {
+        yield return new WaitForSeconds(_time);
+        if (_action != null)
+            _action();
+    }
+
+    public IEnumerator fillCodeLine(GameObject codeLine, Color _color, float _time, Action _action) {
+        Slider _slider = codeLine.transform.Find("Slider").GetComponent<Slider>();
+        Image _fillImage = codeLine.transform.Find("Slider").transform.Find("Fill Area").transform.Find("Fill").GetComponent<Image>();
+        _fillImage.color = _color;
+
+
+        float aux = 0f;
+        while (_slider.value < 1) {
+            _slider.value = fillAnimationCurve.Evaluate(aux / _time);
+            aux += _time * Time.deltaTime;
+            yield return null;
+        }
+        // yield return new WaitForSeconds(_time);
+        // codeLine.GetComponent<Image>().color = _color;
+        try {
+            StartCoroutine(codeLineAlphaRoutine(_fillImage, _time));
+        } catch (System.Exception) {
+            Debug.LogWarning("Error al animar alpha");
+        }
+
+    }
+
+    public IEnumerator codeLineAlphaRoutine(Image _fillImage, float _time) {
+        // _fillImage
+        float aux = 0f;
+        while (_fillImage && _fillImage.color.a > 0f) {
+            _fillImage.color = new Color(_fillImage.color.r, _fillImage.color.g, _fillImage.color.b, 1 - fillAnimationCurve.Evaluate(aux));
+            aux += Time.deltaTime;
+            yield return null;
+        }
+
     }
 
     public void resetCodeColor() {
         List<GameObject> Children = new List<GameObject>();
         int i = 0;
         foreach (Transform child in codeContainer.transform) {
-            child.GetComponent<Image>().color = i % 2 == 0 ? amarillo_1 : amarillo_2;
+            child.transform.Find("Slider").GetComponent<Slider>().value = 0;
+            Image image = child.transform.Find("Slider").transform.Find("Fill Area").transform.Find("Fill").GetComponent<Image>();
+            var temp = image.color;
+            temp.a = 1f;
+            image.color = temp;
+            child.transform.Find("Slider").transform.Find("Background").GetComponent<Image>().color = i % 2 == 0 ? amarillo_1 : amarillo_2;
             i++;
         }
     }
@@ -76,16 +138,10 @@ public class CodeLineManager : MonoBehaviour {
         for (int i = 0; i < codeLines.Count; i++) {
             var temp = codeLines[i].Replace("#", "    ");
             GameObject newLine = Instantiate(linePrefab, codeContainer.transform);
-            newLine.GetComponent<Image>().color = i % 2 == 0 ? amarillo_1 : amarillo_2;
+            newLine.transform.Find("Slider").transform.Find("Background").GetComponent<Image>().color = i % 2 == 0 ? amarillo_1 : amarillo_2;
             newLine.transform.Find("numero").GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
             newLine.transform.Find("codigo").GetComponent<TextMeshProUGUI>().text = temp;
         }
-        // GameObject finalLine = Instantiate(linePrefab, codeContainer.transform);
-        // finalLine.GetComponent<Image>().color = verde;
-        // finalLine.transform.Find("numero").GetComponent<TextMeshProUGUI>().text = (codeLines.Count + 1).ToString();
-        // finalLine.transform.Find("codigo").GetComponent<TextMeshProUGUI>().text = "Presionar <sprite=1> para activar";
-        // Debug.Log("UpdateFocusedObject");
-
     }
 
     public void clearCodeLines() {
@@ -95,6 +151,7 @@ public class CodeLineManager : MonoBehaviour {
         }
         foreach (GameObject child in Children) {
             Destroy(child);
+            // child.SetActive(false);
         }
     }
 }
--- Assets/Scripts/PlaygroundEvents.cs
+++ Assets/Scripts/PlaygroundEvents.cs
@@ -40,38 +40,51 @@ public class PlaygroundEvents : MonoBehaviour {
     }
     public IEnumerator pressButtonARoutine(int _lineIndex, float time = 1f) {
         bool ejecutando = true;
+        int prevLine = _lineIndex;
         while (ejecutando) {
+            Debug.Log("Ejecutando" + prevLine + "   " + _lineIndex);
             switch (_lineIndex) {
                 case 0:
                     if (DoorsList[CONST.A].GetComponent<GenericDoor>().opened) {
-                        CodeLineManager._instance.trySetColorLine(0, green);
+                        CodeLineManager._instance.trySetColorLine(0, green, time);
                         _lineIndex = 1;
                     } else {
                         _lineIndex = 2;
-                        CodeLineManager._instance.trySetColorLine(0, 1, grey);
+                        // CodeLineManager._instance.trySetColorLine(0, 1, grey, time);
                     }
                     break;
                 case 1:
-                    DoorsList[CONST.A].GetComponent<GenericDoor>().openOrClose();
-                    CodeLineManager._instance.trySetColorLine(1, green);
-                    CodeLineManager._instance.trySetColorLine(2, 3, grey);
-                    ejecutando = false;
+                    CodeLineManager._instance.trySetColorLine(1, green, time,
+                    () => {
+                        DoorsList[CONST.A].GetComponent<GenericDoor>().openOrClose();
+                        // ejecutando = false;
+                    });
+                    // CodeLineManager._instance.trySetColorLine(2, 3, grey);
                     break;
                 case 2:
-                    CodeLineManager._instance.trySetColorLine(2, green);
+                    CodeLineManager._instance.trySetColorLine(2, green, time);
                     _lineIndex = 3;
                     break;
                 case 3:
-                    CodeLineManager._instance.trySetColorLine(3, green);
-                    DoorsList[CONST.A].GetComponent<GenericDoor>().openOrClose();
-                    ejecutando = false;
+                    CodeLineManager._instance.trySetColorLine(3, green, time,
+                    () => {
+                        DoorsList[CONST.A].GetComponent<GenericDoor>().openOrClose();
+                        // ejecutando = false;
+                    });
                     break;
                 default:
                     break;
             }
-            yield return new WaitForSeconds(time);
+            yield return new WaitForSeconds(time + 0.1f);
+            if (prevLine == _lineIndex) ejecutando = false;
+            else prevLine = _lineIndex;
         }
+        terminarEjecucion();
 
+    }
+
+    void terminarEjecucion() {
+        Debug.Log("ejecucion terminada");
     }
 
     public void pressButtonB() {
